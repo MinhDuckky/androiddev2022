cf52226bfb326d141cd707e652740c605bdd1f59
MinhDuckky <81146050+MinhDuckky@users.noreply.github.com>
2021-10-13 11:57:58 +0700

    Practical Work 2

---
+++ PW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,48 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("Weather", "onCreate() is called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weather", "onStart() is called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weather", "onResume() is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Weather", "onPause() is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weather", "onStop() is called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather", "onDestroy() is called");
+    }
+
+}
