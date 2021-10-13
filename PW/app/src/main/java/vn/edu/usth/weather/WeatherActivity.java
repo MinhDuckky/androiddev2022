06a0bfd15f4514124ee84e5d5b3b0d345120b866
MinhDuckky <81146050+MinhDuckky@users.noreply.github.com>
2021-10-13 12:30:31 +0700

    Practical Work 3

--- PW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ PW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -12,6 +12,9 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
         Log.i("Weather", "onCreate() is called");
     }
 
